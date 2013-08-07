5540d46d77b929fe9f75a1df9797300086a5aeed
Justin Lambert <jlambert@eml.cc>
2013-08-07 09:53:23 -0600

    allow puppet to not manage the service

--- spec/classes/tomcat_service_spec.rb
+++ spec/classes/tomcat_service_spec.rb
@@ -10,9 +10,13 @@ describe 'tomcat::service', :type => :class do
     'enable'  => true
   ) }
 
+  context 'not managed' do
+    let(:params) { { :manage => false } }
+    it { should_not contain_service('tomcat') }
+  end
+
   context 'sensu monitoring' do
     let(:params) { { :monitoring => 'sensu' } }
-
     it { should contain_class('tomcat::monitoring::sensu') }
   end
 end
